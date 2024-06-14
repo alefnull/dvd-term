f3d3991156e460efe496b51cb2f1636d21465a9e
alefnull <nullalef@gmail.com>
2024-06-14 00:50:42 -0400

    replace anyhow with basic errors (also, formatting)

--- src/app.rs
+++ src/app.rs
@@ -1,80 +1,103 @@
-use crate::util::{fig_size, figlet, term_size, Vec2};
-use anyhow::Result;
+use crate::util::{fig_size, figlet, term_size, Result, Vec2};
 use crossterm::{
   cursor::{Hide, MoveTo, Show},
   event::{poll, read, Event, KeyEvent},
-  execute, queue,
+  execute,
+  queue,
   style::{Color, Print, SetForegroundColor},
   terminal::{
-    disable_raw_mode, enable_raw_mode, Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen,
+    disable_raw_mode,
+    enable_raw_mode,
+    Clear,
+    ClearType,
+    EnterAlternateScreen,
+    LeaveAlternateScreen,
   },
 };
 use figlet_rs::FIGfont;
 use rand::Rng;
 use std::{
+  fs::read_to_string,
   io::{BufWriter, Write},
+  path::Path,
   time::Duration,
 };
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq,)]
 pub struct Cell {
   pub color: u8,
-  pub char: char,
+  pub char:  char,
 }
 
 pub struct App {
-  pub target: BufWriter<std::io::Stdout>,
-  pub input: String,
-  pub font: FIGfont,
+  pub target:      BufWriter<std::io::Stdout,>,
+  pub input:       String,
   pub logo_string: String,
-  pub logo_size: Vec2,
+  pub logo_size:   Vec2,
   pub canvas_size: Vec2,
-  pub color: u8,
-  pub speed: u64,
-  pub random: bool,
-  pub position: Vec2,
-  pub direction: Vec2,
-  pub running: bool,
-  pub plain: bool,
-  pub art: bool,
+  pub color:       u8,
+  pub speed:       u64,
+  pub random:      bool,
+  pub position:    Vec2,
+  pub direction:   Vec2,
+  pub running:     bool,
+  pub plain:       bool,
 }
 
 impl App {
   pub fn new(
-    input_text: String,
-    font: FIGfont,
-    color: u8,
-    random: bool,
-    speed: u64,
-    plain: bool,
-    art: bool,
-  ) -> Result<Self> {
-    let fig_str = figlet(&input_text, &font)?;
-    let fig_size = fig_size(&fig_str)?;
+    input_text: String, font: FIGfont, color: u8, random: bool, speed: u64,
+    plain: bool, art_path: String,
+  ) -> Result<Self,> {
+    let mut logo_str = figlet(&input_text, &font,)?;
+    let mut logo_size = fig_size(&logo_str,)?;
+
+    let art = !art_path.is_empty();
+    if art {
+      if !Path::new(&art_path,).exists() {
+        return Err(std::io::Error::new(
+          std::io::ErrorKind::NotFound,
+          format!("File not found: {}", art_path,),
+        ),);
+      }
+      else {
+        let art_path = Path::new(&art_path,);
+        logo_str = read_to_string(art_path,)?;
+        logo_size = fig_size(&logo_str,)?;
+      }
+    }
+    if art {
+      logo_str = read_to_string(art_path,)?;
+      logo_size = fig_size(logo_str.as_str(),)?;
+    }
     Ok(Self {
       input: input_text,
-      font,
       color,
       random,
       speed,
-      target: BufWriter::new(std::io::stdout()),
-      logo_string: fig_str,
+      target: BufWriter::new(std::io::stdout(),),
+      logo_string: logo_str,
       canvas_size: term_size()?,
-      logo_size: fig_size,
-      position: Vec2::rand(term_size()?.x, term_size()?.y, fig_size.x, fig_size.y),
+      logo_size,
+      position: Vec2::rand(
+        term_size()?.x,
+        term_size()?.y,
+        logo_size.x,
+        logo_size.y,
+      ),
       direction: Vec2::rand_dir(),
       running: true,
       plain,
-      art,
-    })
+    },)
   }
 
-  pub fn update(&mut self) -> Result<()> {
+  pub fn update(&mut self,) -> Result<(),> {
     self.canvas_size = term_size()?;
 
     let plain = if self.plain {
       true
-    } else {
+    }
+    else {
       self.logo_size.x >= self.canvas_size.x - (self.logo_size.x / 2)
         || self.logo_size.y >= self.canvas_size.y - (self.logo_size.y / 2)
     };
@@ -86,7 +109,10 @@ impl App {
         bounce = true;
         self.position.x = 0;
         self.direction.x = -self.direction.x;
-      } else if self.position.x + self.direction.x + self.logo_size.x >= self.canvas_size.x {
+      }
+      else if self.position.x + self.direction.x + self.logo_size.x
+        >= self.canvas_size.x
+      {
         bounce = true;
         self.position.x = self.canvas_size.x - self.logo_size.x - 1;
         self.direction.x = -self.direction.x;
@@ -96,17 +122,24 @@ impl App {
         bounce = true;
         self.position.y = 0;
         self.direction.y = -self.direction.y;
-      } else if self.position.y + self.direction.y + self.logo_size.y > self.canvas_size.y {
+      }
+      else if self.position.y + self.direction.y + self.logo_size.y
+        > self.canvas_size.y
+      {
         bounce = true;
         self.position.y = self.canvas_size.y - self.logo_size.y;
         self.direction.y = -self.direction.y;
       }
-    } else {
+    }
+    else {
       if self.position.x + self.direction.x < 0 {
         bounce = true;
         self.position.x = 0;
         self.direction.x = -self.direction.x;
-      } else if self.position.x + self.direction.x + self.input.len() as i32 >= self.canvas_size.x {
+      }
+      else if self.position.x + self.direction.x + self.input.len() as i32
+        >= self.canvas_size.x
+      {
         bounce = true;
         self.position.x = self.canvas_size.x - self.input.len() as i32 - 1;
         self.direction.x = -self.direction.x;
@@ -116,7 +149,8 @@ impl App {
         bounce = true;
         self.position.y = 0;
         self.direction.y = -self.direction.y;
-      } else if self.position.y + self.direction.y > self.canvas_size.y - 1 {
+      }
+      else if self.position.y + self.direction.y > self.canvas_size.y - 1 {
         bounce = true;
         self.position.y = self.canvas_size.y - 1;
         self.direction.y = -self.direction.y;
@@ -127,18 +161,19 @@ impl App {
     self.position.y += self.direction.y;
 
     if self.random && bounce {
-      self.color = rand::thread_rng().gen_range(0..=255);
+      self.color = rand::thread_rng().gen_range(0..=255,);
     }
 
-    Ok(())
+    Ok((),)
   }
 
-  pub fn draw(&mut self) -> Result<()> {
+  pub fn draw(&mut self,) -> Result<(),> {
     queue!(self.target, Clear(ClearType::All),)?;
 
     let plain = if self.plain {
       true
-    } else {
+    }
+    else {
       self.logo_size.x >= self.canvas_size.x - (self.logo_size.x / 2)
         || self.logo_size.y >= self.canvas_size.y - (self.logo_size.y / 2)
     };
@@ -149,9 +184,10 @@ impl App {
         SetForegroundColor(Color::AnsiValue(self.color)),
         Print(self.input.as_str())
       )?;
-    } else {
-      for (i, line) in self.logo_string.lines().enumerate() {
-        let pos = Vec2::new(self.position.x, self.position.y + i as i32);
+    }
+    else {
+      for (i, line,) in self.logo_string.lines().enumerate() {
+        let pos = Vec2::new(self.position.x, self.position.y + i as i32,);
 
         queue!(
           self.target,
@@ -164,36 +200,36 @@ impl App {
 
     self.target.flush()?;
 
-    Ok(())
+    Ok((),)
   }
 
-  pub fn handle_input(&mut self) -> Result<()> {
+  pub fn handle_input(&mut self,) -> Result<(),> {
     let target_frame_time = 1000 / self.speed;
 
-    if poll(Duration::from_millis(target_frame_time))? {
+    if poll(Duration::from_millis(target_frame_time,),)? {
       match read()? {
         | Event::Key(KeyEvent {
-          code: crossterm::event::KeyCode::Char('q'),
+          code: crossterm::event::KeyCode::Char('q',),
           ..
-        }) => self.running = false,
+        },) => self.running = false,
         | Event::Key(KeyEvent {
           code: crossterm::event::KeyCode::Esc,
           ..
-        }) => self.running = false,
+        },) => self.running = false,
         | Event::Key(KeyEvent {
-          code: crossterm::event::KeyCode::Char('c'),
+          code: crossterm::event::KeyCode::Char('c',),
           modifiers: crossterm::event::KeyModifiers::CONTROL,
           state: crossterm::event::KeyEventState::NONE,
           kind: crossterm::event::KeyEventKind::Press,
-        }) => self.running = false,
+        },) => self.running = false,
         | _ => (),
       }
     }
 
-    Ok(())
+    Ok((),)
   }
 
-  pub fn run(&mut self) -> Result<()> {
+  pub fn run(&mut self,) -> Result<(),> {
     enable_raw_mode()?;
     execute!(self.target, EnterAlternateScreen, Hide,)?;
 
@@ -206,6 +242,6 @@ impl App {
     execute!(self.target, Show, LeaveAlternateScreen)?;
     disable_raw_mode()?;
 
-    Ok(())
+    Ok((),)
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,115 @@
-use crate::util::fig_size;
-use anyhow::Result;
-use clap::{arg, command, value_parser, ArgAction};
+use crate::util::{Result, DEFAULT_FONT};
+use clap::{arg, value_parser, ArgAction, Command};
 use figlet_rs::FIGfont;
-use once_cell::sync::Lazy;
-use std::fs::read_to_string;
 
 mod app;
 mod util;
 
-static DEFAULT_FONT: &str = include_str!("../assets/hash3d.flf");
-
-static MATCHES: Lazy<clap::ArgMatches> = Lazy::new(|| {
-  command!()
+fn main() -> Result<(),> {
+  let cmd = Command::new("dvd-term")
+    .version(env!("CARGO_PKG_VERSION"))
+    .about("A bouncing ASCII art DVD logo (or custom text) for the terminal.")
     .help_template("\n
 {before-help}{name} {version}
 {about-with-newline}
 {usage-heading} {usage}
 
 {all-args}{after-help}\n")
-    .about("A bouncing ASCII art DVD logo (or custom text) for the terminal.")
-    .arg(
-      arg!(-t --text <TEXT> "The custom text to use. Defaults to \"DVD\"")
-        .value_parser(value_parser!(String))
-        .required(false)
-        .action(ArgAction::Set),)
-    .arg(
-      arg!(-f --font <FONT_PATH> "Specify the path of the figlet font to use")
-        .value_parser(value_parser!(String))
-        .required(false)
-        .action(ArgAction::Set),)
-    .arg(
-      arg!(-c --color <COLOR> "Initial logo color code (0-255). Defaults to white (15). (https://ss64.com/bash/syntax-colors.html)")
-        .value_parser(value_parser!(u8))
-        .required(false)
-        .action(ArgAction::Set),)
-    .arg(
-      arg!(-r --random "If included, logo will randomize color when it bounces")
-        .value_parser(value_parser!(bool))
-        .required(false)
-        .action(ArgAction::SetTrue),)
-    .arg(
-      arg!(-s --speed <SPEED> "The speed of the logo (how many 'cells' to move per second). Defaults to 15")
-        .value_parser(value_parser!(u32))
-        .required(false)
-        .action(ArgAction::Set),)
-    .arg(
-      arg!(-p --plain "If included, logo will be displayed in plain text instead of converted to ASCII art")
-        .value_parser(value_parser!(bool))
-        .required(false)
-        .action(ArgAction::SetTrue),)
-    .arg(
-      arg!(-a --art <ART_PATH> "Specify the path of a plain text file with the ASCII art to display")
-        .value_parser(value_parser!(String))
-        .required(false)
-        .action(ArgAction::Set),)
-    .get_matches()
-});
+    .arg(arg!(-t --text <TEXT> "The custom text to use. Defaults to \"DVD\"")
+      .value_parser(value_parser!(String))
+      .required(false)
+      .action(ArgAction::Set))
+    .arg(arg!(-f --font <FONT_PATH> "Specify the path of the figlet font to use")
+      .value_parser(value_parser!(String))
+      .required(false)
+      .action(ArgAction::Set))
+    .arg(arg!(-c --color <COLOR> "Initial logo color code (0-255). Defaults to white (15). (https://ss64.com/bash/syntax-colors.html)")
+      .value_parser(value_parser!(u8))
+      .required(false)
+      .action(ArgAction::Set))
+    .arg(arg!(-r --random "If included, logo will randomize color when it bounces")
+      .value_parser(value_parser!(bool))
+      .required(false)
+      .action(ArgAction::SetTrue))
+    .arg(arg!(-s --speed <SPEED> "The speed of the logo (how many 'cells' to move per second). Defaults to 15")
+      .value_parser(value_parser!(u64))
+      .required(false)
+      .action(ArgAction::Set))
+    .arg(arg!(-p --plain "If included, logo will be displayed in plain text instead of converted to ASCII art")
+      .value_parser(value_parser!(bool))
+      .required(false)
+      .action(ArgAction::SetTrue))
+    .arg(arg!(-a --art <ART_PATH> "Specify the path of a plain text file with the ASCII art to display")
+      .value_parser(value_parser!(String))
+      .required(false)
+      .action(ArgAction::Set));
 
-fn main() -> Result<()> {
-  let input_text = if let Some(input_text) = MATCHES.get_one::<String>("text") {
+  let matches = cmd.clone().get_matches();
+
+  let input_text = if let Some(input_text,) = matches.get_one::<String>("text",)
+  {
     input_text.to_string()
-  } else {
+  }
+  else {
     "DVD".to_string()
   };
 
-  let font_path = if let Some(font_path) = MATCHES.get_one::<String>("font") {
+  let font_path = if let Some(font_path,) = matches.get_one::<String>("font",) {
     font_path.to_string()
-  } else {
+  }
+  else {
     "".to_string()
   };
 
-  let default_font = FIGfont::from_content(DEFAULT_FONT).unwrap_or(FIGfont::standard().unwrap());
+  let default_font = FIGfont::from_content(DEFAULT_FONT,)
+    .unwrap_or(FIGfont::standard().unwrap(),);
 
   let font = if font_path.is_empty() {
     default_font
-  } else {
-    FIGfont::from_file(&font_path).unwrap_or(default_font)
+  }
+  else {
+    FIGfont::from_file(&font_path,).unwrap_or(default_font,)
   };
 
-  let color = if let Some(color) = MATCHES.get_one::<u8>("color") {
+  let color = if let Some(color,) = matches.get_one::<u8>("color",) {
     *color
-  } else {
+  }
+  else {
     15
   };
 
-  // let random = MATCHES.get_one::<bool>("random").is_some();
-  let random = if let Some(random) = MATCHES.get_one::<bool>("random") {
+  let random = if let Some(random,) = matches.get_one::<bool>("random",) {
     *random
-  } else {
+  }
+  else {
     false
   };
 
-  let speed = if let Some(speed) = MATCHES.get_one::<u32>("speed") {
-    *speed as u64
-  } else {
+  let speed = if let Some(speed,) = matches.get_one::<u64>("speed",) {
+    *speed
+  }
+  else {
     8
   };
 
-  // let plain = MATCHES.get_one::<bool>("plain").is_some();
-  let plain = if let Some(plain) = MATCHES.get_one::<bool>("plain") {
+  let plain = if let Some(plain,) = matches.get_one::<bool>("plain",) {
     *plain
-  } else {
+  }
+  else {
     false
   };
 
-  let art_path = if let Some(art_path) = MATCHES.get_one::<String>("art") {
+  let art_path = if let Some(art_path,) = matches.get_one::<String>("art",) {
     art_path.to_string()
-  } else {
+  }
+  else {
     "".to_string()
   };
 
-  let art = !art_path.is_empty();
-
-  let mut app = app::App::new(input_text, font, color, random, speed, plain, art)?;
-
-  if art {
-    app.logo_string = read_to_string(art_path)?;
-    app.logo_size = fig_size(&app.logo_string)?;
-  }
+  let mut app =
+    app::App::new(input_text, font, color, random, speed, plain, art_path,)?;
 
   app.run()?;
 
-  Ok(())
+  Ok((),)
 }
